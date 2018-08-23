5b8c59d22513cd9ff862e4e2ce2d0b6e2e759f40
Dylan <deejtonks@gmail.com>
2018-08-23 19:30:15 +1200

    Added search functionality to consultants view

--- user/views/consultant.py
+++ user/views/consultant.py
@@ -8,8 +8,16 @@ class Listing(View):
 
     def get(self, request):
 
-        # get all consultants
-        consultants = Consultant.objects.all()
+        # search? yes
+        try:
+            # if search does not exist it will return a DictValueError
+            search_term = request.GET['search']
+            consultants = Consultant.search_consultants(search_term)
+
+        # no
+        except:
+            # get all consultants
+            consultants = Consultant.objects.all()
 
         # divide consultants by five per page
         paginator = Paginator(consultants, 5)
