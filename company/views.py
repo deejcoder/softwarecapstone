347a759aaf89483d4a6d5f6b2af8abc0486e40a4
SioThePaladin <41462077+SioThePaladin@users.noreply.github.com>
2018-08-30 11:38:11 +1200

    Update views.py

--- company/views.py
+++ company/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#Temp view for company application form template
+
+def company(request):
+    label_list = ["IRD number", "Company name", "Street address", "Postal address", "Contact details", "Website", ]
+    return render(request, 'company/comp_appform.html', {'list': label_list})
