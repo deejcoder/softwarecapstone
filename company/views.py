21a869a912cee916bc3b352515587e8c561fbf83
SioThePaladin <41462077+SioThePaladin@users.noreply.github.com>
2018-09-06 10:07:49 +1200

    Update views.py

--- company/views.py
+++ company/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import CompanyApplicationForm
 
 # Create your views here.
 
@@ -7,3 +8,26 @@ from django.shortcuts import render
 def company(request):
     label_list = ["IRD number", "Company name", "Street address", "Postal address", "Contact details", "Website", ]
     return render(request, 'company/comp_appform.html', {'list': label_list})
+
+
+# Rough view for comp_appform.html
+class ApplyCompany(View):
+    def get(self, request):
+        """
+        User wants to apply
+        """
+        form = CompanyApplicationForm()
+        return render(request, 'company/comp_appform.html', {'form': form})
+
+    def post(self, request):
+        """
+        User submits the application form
+        """
+        form = CompanyApplicationForm(request.POST)
+        if form.is_valid():
+            app = form.save(commit=False)
+            app.user = request.user
+            app.save()
+
+        return redirect('/company/')
+    
