3e8e3dd00a1abadd4ec1807584e3f84854a0b12e
SioThePaladin <41462077+SioThePaladin@users.noreply.github.com>
2018-09-06 10:05:50 +1200

    Create forms.py

---
+++ company/forms.py
@@ -0,0 +1,51 @@
+from django.forms import ModelForm
+from .models import Company
+import pickle
+from django import forms
+
+
+class AddressWidget(forms.widgets.MultiWidget):
+    def __init__(self, attrs=None):
+        widgets = [forms.TextInput(),
+                   forms.TextInput()]
+        super(AddressWidget, self).__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            return pickle.loads(value)
+        else:
+            return ['', '']
+
+
+class AddressField(forms.fields.MultiValueField):
+    widget = AddressWidget
+
+    def __init__(self, *args, **kwargs):
+        list_fields = [forms.fields.CharField(max_length=40),
+                       forms.fields.CharField(max_length=40)]
+        super(AddressField, self).__init__(list_fields, *args, **kwargs)
+
+    def compress(self, values):
+        return pickle.dumps(values)
+
+
+class CompanyApplicationForm(ModelForm):
+    address = AddressField()
+    address.label = "Address"
+
+    class Meta:
+        model = Company
+
+        fields = [
+            'name',
+            'ird_no',
+            'avatar',
+            'contact_phone',
+            'contact_email',
+            'website',
+            'summer_students',
+            'industry',
+            'type_of_business',
+            'size',
+            'specialist_area'
+        ]
