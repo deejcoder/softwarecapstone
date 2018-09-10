8d6c5e3182a6ab20d4cc23d4c36f61d9b044c1bf
SioThePaladin <41462077+SioThePaladin@users.noreply.github.com>
2018-09-10 20:04:41 +1200

    Update settings.py

--- techpalmy/settings.py
+++ techpalmy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'widget_tweaks',
     'user',
     'event',
     'company',
