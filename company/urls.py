4c887f4ffa6e809fbbe980892f15d1ec3d82d37e
SioThePaladin <41462077+SioThePaladin@users.noreply.github.com>
2018-09-10 20:10:10 +1200

    Update urls.py

--- company/urls.py
+++ company/urls.py
@@ -5,4 +5,5 @@ app_name = "company"
 
 urlpatterns = [
     path('company/apply', views.ApplyCompany.as_view(), name='company_application'),
+    path('company/profile/edit', views.edit_comp, name='company_application'),
 ]
