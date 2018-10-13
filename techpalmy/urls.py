ad1e145c0e8e65da642d8c8e19e13f25d3f85e64
Dylan <deejtonks@gmail.com>
2018-10-13 20:05:35 +1300

    Debugging tool for Django (side pane on left when loading pages)

--- techpalmy/urls.py
+++ techpalmy/urls.py
@@ -31,3 +31,9 @@ urlpatterns = [
     path('events/', include('event.urls')),
     path('admin/', include('dashboard.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
