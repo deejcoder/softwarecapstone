a02af4d2955ce9460252f52163045639ab3956f4
Dylan <deejtonks@gmail.com>
2018-10-14 19:58:56 +1300

    Finished jobs model & tested it

--- jobs/models.py
+++ jobs/models.py
@@ -1,24 +1,24 @@
-from datetime import timedelta
+import datetime
 
 from django.contrib.postgres.search import SearchQuery, SearchVector
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
 
-from entity.models import Company
+from entity.models.company import Company
 
 
 class Job(models.Model):
 
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='jobs')
-    title = models.CharField(required=True, max_length=30)
+    title = models.CharField(max_length=30)
     description = models.TextField(max_length=500*5.1)  # average word length=5.1
     contact_email = models.EmailField()
     contact_phone = models.CharField(max_length=15, validators=[
         RegexValidator(regex='^[0-9]*$', message="A phone number can only contain numbers.")
     ])
-    expiry = models.DateTimeField(default=(timezone.now+timedelta(days=14)))
-    external_link = models.CharField(max_length=2083)  # IE has URL max length=2083
+    expiry = models.DateTimeField(default=(timezone.now()+datetime.timedelta(days=14)))
+    external_link = models.CharField(blank=True, max_length=2083, default=None)  # IE has URL max length=2083
 
     @classmethod
     def search_jobs(cls, term: str):
