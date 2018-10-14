364e6e1abab82966977c45a6e7c8a8c066ac5b9d
Dylan <deejtonks@gmail.com>
2018-10-14 21:10:52 +1300

    Events model

--- event/models.py
+++ event/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from entity.models import Entity
 
-# Create your models here.
+
+class Event(models.Model):
+
+    entity = models.ForeignKey(Entity, on_delete=models.CASCADE, related_name='event')
+    title = models.CharField(max_length=80)
+    time = models.DateTimeField()
+    location = models.CharField(max_length=80)
+    description = models.TextField(max_length=500*5.1)  # 5.1 = average word length
+
+    class Meta:
+        ordering = ('-time')
+
+
