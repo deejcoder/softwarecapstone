c58d058be76b4a6764f67ca1b033a41ed3b2e9be
SioThePaladin <41462077+SioThePaladin@users.noreply.github.com>
2018-10-15 14:37:59 +1300

    Add files via upload

---
+++ event/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Event
+
+
+class CreateEventForm(ModelForm):
+
+    class Meta:
+        model = Event
+
+        fields = {
+            'entity',
+            'title',
+            'date',
+            'time',
+            'location',
+            'description'
+        }
+
+        widgets = {
+            'description': forms.Textarea,
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+            'date': forms.DateInput(attrs={'type': 'date'}),
+        }
