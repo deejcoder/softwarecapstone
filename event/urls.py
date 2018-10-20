55b7ef9df4f7f8b30d2d7cfcab7c132d4ca03ed9
SioThePaladin <41462077+SioThePaladin@users.noreply.github.com>
2018-10-20 13:59:59 +1300

    Update urls.py

--- event/urls.py
+++ event/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.events, name='events_listing'),
     path('create', views.CreateEvent.as_view(), name='create_event'),
     path('<event_title>-<event_id>/', views.EventDetails.as_view(), name='event_details'),
+    path('<event_title>-<event_id>/remove', views.remove_event, name="remove_event")
 ]
