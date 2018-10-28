31f010068bcbef84200ad0462c6aff92c6a9967e
SioThePaladin <wind.striker.78@gmail.com>
2018-10-28 20:10:43 +1300

    Template updates
    General fixes/updates for misc. templates

--- user/urls.py
+++ user/urls.py
@@ -8,8 +8,8 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     path('user/<username>/', views.Profile.as_view(), name='user_profile'),
-    path('user/<username>/edit', views.EditProfile.as_view(), name='user_profile_edit'),
+    path('user/<username>/edit/', views.EditProfile.as_view(), name='user_profile_edit'),
     path('consultants/', views.Listing.as_view()),
-    path('consultants/apply', views.Apply.as_view()),
+    path('consultants/apply/', views.Apply.as_view()),
     path('register/', views.Register.as_view(), name="register"),
 ]
